46df6ed43c42d6db0b4d943489f7c2775faba926
jwang9392 <jwang9392@gmail.com>
2020-01-13 14:53:52 -0500

    Finished Sessions Controller and tested with postman

--- reddit_clone/app/controllers/sessions_controller.rb
+++ reddit_clone/app/controllers/sessions_controller.rb
@@ -1,15 +1,29 @@
 class SessionsController < ApplicationController
 
-  def new
+  skip_before_action :verify_authenticity_token
+  before_action :ensure_logged_in, only: [:destroy]
 
+  def new
+    render :new
   end
 
   def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
 
+    if @user
+      log_in!(@user)
+      redirect_to users_url
+    else
+      flash.now[:error] = ["Invalid username or password. Try again"]
+      render :new
+    end
   end
 
   def destroy
-
+    log_out!
   end
 
 end
