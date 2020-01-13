41dd555f74dbabe29edf758070dab52a59b728e4
jwang9392 <jwang9392@gmail.com>
2020-01-13 18:42:13 -0500

    Completed Subs Controller and created corresponding routes

--- reddit_clone/config/routes.rb
+++ reddit_clone/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :new, :create, :show]
+  resources :subs, except: [:destroy]
 
   resource :session, only: [:new, :create, :destroy]
 end
